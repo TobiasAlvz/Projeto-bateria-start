c4f9be6963d53d7f3411be314118f8708b1d4b53
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-05-31 10:16:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ buttons.forEach(function (button) {
 function handlerClick() {
   alert("click");
 }
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function mutly(num1, num2) {
+  return num1 * num2;
+}
+
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
+
+console.log(
+  calculator(10, 5, add),
+  calculator(10, 5, sub),
+  calculator(10, 5, mutly),
+  calculator(10, 5, div)
+);
