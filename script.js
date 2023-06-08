2de1290f3a0eb0ca9c05d9eeaf918280481d5e78
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-06-08 17:42:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("sim");
+function houseKeeper(age, name, clean) {
+  this.age = age;
+  this.name = name;
+  this.clean = clean;
+  this.fullClear = function () {
+    alert("Tudo está limpo");
+  };
+}
+
+let houseKeeper1 = new houseKeeper(12, "Isa", ["room", "door"], fullClear());
