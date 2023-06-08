e3bb933c93fb07eca6725ed95739a17f2334633e
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-06-08 17:42:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,47 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(function (button) {
   button.addEventListener("click", soundClick);
 });
-
 function soundClick() {
-  let sound = new Audio("./sounds/tom-1.mp3");
-  sound.play();
-  this.style.color = "red";
+  let buttonInnerHTML = this.innerHTML;
+  makeSounds(buttonInnerHTML);
 }
 
-const governanta = {
-  nome: "ad",
-  age: 19,
-  work: true,
-};
+document.addEventListener("keydown", function (event) {
+  makeSounds(event.key);
+});
 
-console.log(governanta);
+function makeSounds(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      let kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      break;
+  }
+}
